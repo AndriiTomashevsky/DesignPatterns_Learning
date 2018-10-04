6990341303aa5a2639aeef4729d37f3d4d57ae81
Andrii Tomashevskyi <tomashevskiy.a@gmail.com>
2018-10-04 12:50:05 +0300

    Chapter 1. Intro to Design Patterns: Welcome to Design Patterns, It started with a simple SimUDuck app

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,16 +1,55 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DesignPatterns
 {
+    public abstract class Duck
+    {
+        public abstract void Display();
+        public void Swim() { }
+        public virtual void Fly() { }
+        public virtual void Quack() { }
+    }
+
+    public class MallardDuck : Duck
+    {
+        public override void Display() { }
+    }
+
+    public class ReadheadDuck : Duck
+    {
+        public override void Display() { }
+    }
+
+    public class RubberDuck : Duck
+    {
+        public override void Display() { }
+        public override void Quack() { }
+        public override void Fly() { }
+    }
+
+    public class DecoyDuck : Duck
+    {
+        public override void Display() { }
+        public override void Quack()
+        {
+            // override to fo nothing 
+        }
+        public override void Fly()
+        {
+            // override to fo nothing
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MallardDuck mallardDuck = new MallardDuck();
+            mallardDuck.Quack();
+
+            Duck rubberDuck = new RubberDuck();
+            mallardDuck.Quack();
+
             Console.ReadKey();
         }
     }
