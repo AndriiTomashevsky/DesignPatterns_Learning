1ec84fd54233f3d8b993f0b9fb01ecaf6df5bb83
Andrii Tomashevskyi <tomashevskiy.a@gmail.com>
2018-10-04 17:09:05 +0300

    Separating what changes from what stays the same (The FIRST design principle), Designing the Duck Behaviors (The SECOND design principle)

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,50 +1,90 @@
 using System;
+using System.Collections.Generic;
 
+//                                  FIRST DESIGN PRINCIPLE
+
+//Identify the aspects of your application that vary and separate them from what stays the
+//same.
+//                                  SECOND DESIGN PRINCIPLE
+
+//Program to an interface, not an implementation.
 namespace DesignPatterns
 {
-    interface IFlyable { void Fly(); }
-    interface IQuackable { void Quack(); }
+    public interface IFlyBehavior { void Fly(); }
+    public interface IQuackBehavior { void Quack(); }
+
+    class FlyWithWings : IFlyBehavior { public void Fly() { Console.WriteLine("I'm flying!"); } }
+    class FlyNoWay : IFlyBehavior { public void Fly() { Console.WriteLine("I can't fly"); } }
+
+    class QuackClass : IQuackBehavior { public void Quack() { Console.WriteLine("Quack"); } }
+    class Squeak : IQuackBehavior { public void Quack() { Console.WriteLine("Squeak"); } }
+    class MuteQuack : IQuackBehavior { public void Quack() { Console.WriteLine("<< Silence >>"); } }
 
     public abstract class Duck
     {
+        public IFlyBehavior flyBehavior;
+        public IQuackBehavior quackBehavior;
+
+        public void PerformFly()
+        {
+            flyBehavior.Fly();
+        }
+
+        public void PerformQuack()
+        {
+            quackBehavior.Quack();
+        }
+
         public void Swim() { Console.WriteLine("All ducks float, even decoys!"); }
         public abstract void Display();
     }
 
-    public class MallardDuck : Duck, IFlyable, IQuackable
+    public class MallardDuck : Duck
     {
         public override void Display() { }
-        public void Fly() { }
-        public void Quack() { }
+        public MallardDuck()
+        {
+            quackBehavior = new QuackClass();
+            flyBehavior = new FlyWithWings();
+        }
     }
 
-    public class ReadheadDuck : Duck, IFlyable, IQuackable
+    public class ReadheadDuck : Duck
     {
         public override void Display() { }
-        public void Fly() { }
-        public void Quack() { }
+        public ReadheadDuck()
+        {
+            quackBehavior = new QuackClass();
+            flyBehavior = new FlyWithWings();
+        }
     }
 
-    public class RubberDuck : Duck, IQuackable
+    public class RubberDuck : Duck
     {
         public override void Display() { }
-        public void Quack() { }
-
+        public RubberDuck()
+        {
+            quackBehavior = new Squeak();
+            flyBehavior = new FlyNoWay();
+        }
     }
-
     public class DecoyDuck : Duck
     {
         public override void Display() { }
+        public DecoyDuck()
+        {
+            quackBehavior = new MuteQuack();
+            flyBehavior = new FlyNoWay();
+        }
     }
 
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            MallardDuck mallardDuck = new MallardDuck();
-            Duck rubberDuck = new RubberDuck();
-            mallardDuck.Quack();
-            //rubberDuck.Quack();
+            Duck mallard = new MallardDuck();
+            mallard.PerformFly();
+            mallard.PerformQuack();
 
             Console.ReadKey();
         }
